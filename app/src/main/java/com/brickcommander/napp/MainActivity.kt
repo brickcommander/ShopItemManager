030321979d4b3b2f9ea2d13df0d4d356c76d09ff
brickcommander <pawan961746@gmail.com>
2024-11-09 19:24:58 +0530

    Feat: Created Main Activity

--- app/src/main/java/com/brickcommander/napp/MainActivity.kt
+++ app/src/main/java/com/brickcommander/napp/MainActivity.kt
@@ -1,23 +1,54 @@
 package com.brickcommander.napp
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.brickcommander.napp.ui.theme.NappTheme
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, ShowItemsActivity::class.java))
-        finish()
+        setContentView(R.layout.home_activity)
+
+        // Get the existing ActionBar
+        supportActionBar?.apply {
+            title = "Shop Manager" // Set custom title
+        }
+
+        // Create a list of demo items
+        val demoItems = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+
+        // Get the ListView
+        val listView: ListView = findViewById(R.id.list_of_items)
+
+        // Create an ArrayAdapter to bind the demo items to the ListView
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, demoItems)
+
+        // Set the adapter to the ListView
+        listView.adapter = adapter
+    }
+
+    // Inflate custom menu in ActionBar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    // Handle ActionBar button clicks
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add -> {
+                Toast.makeText(this, "Add clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_sortBy -> {
+                Toast.makeText(this, "Sort By clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
